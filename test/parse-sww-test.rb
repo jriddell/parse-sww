3d212596163baddb9e164a162a95d00bdff48da2
Jonathan Riddell <jr@jriddell.org>
2020-02-16 20:24:39 +0000

    add a new test for several in a row

--- test/parse-sww-test.rb
+++ test/parse-sww-test.rb
@@ -34,6 +34,22 @@ class ParseSwwTest < MiniTest::Test
     assert_equal('3/4', riverEntry.grade)
   end
 
+  def test_parse_html_file_multirivers1
+    parseSww = ParseSww.new('/home/jr/src/parse-sww/parse-sww/test/data/multirivers1/')
+    parseSww.get_html_files()
+    assert_equal(["multirivers1.html"], parseSww.htmlFiles)
+    parseSww.parse_html_files()
+    assert_equal(3, parseSww.riverEntries.length)
+    riverEntry = parseSww.riverEntries[0]
+    assert_equal('Helmsdale', riverEntry.name)
+    assert_equal('John Ross, Gary Smith, Colin Matheson and Ron Cameron', riverEntry.contributor)
+    assert_equal('3/4(4+)', riverEntry.grade)
+    riverEntry = parseSww.riverEntries[1]
+    assert_equal('Kilphedir Burn', riverEntry.name)
+    assert_equal('Colin Matheson', riverEntry.contributor)
+    assert_equal('4(5)', riverEntry.grade)
+  end
+
 =begin
   def test_parse_html_file_section1
     parseSww = ParseSww.new('/home/jr/src/parse-sww/parse-sww/test/data/section1/')
